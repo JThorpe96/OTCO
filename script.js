6476301e6613df25c266dfc8e16e8a41bf92f7c7
Jake Thorpe <jakethorpe@ukcows.com>
2026-02-11 10:04:47 +0000

    Update sendLead function

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function saveOffline(data) {
 
 async function sendLead(data) {
     return fetch(ENDPOINT, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
+        method: "POST",
+        body: JSON.stringify(data),
+        mode: "cors"
     });
 }
 
